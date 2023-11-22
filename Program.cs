8f5e8528ec3df214036ae06d8cf5b567861a38a2
Paweł Łęczycki <pawel@msd.info.pl>
2023-11-22 11:41:54 +0100

    obsługa

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
 ﻿
 
+using Altkom._22_24._11._2023.DesignPrinciples;
+
+CustomerService customerService = new CustomerService();
+PaymentService paymentService = new PaymentService();
+
+
+
+var id = 232;
+
+var customer = customerService.FindById(id);
+paymentService.Fund(customer.PaymentAccount, 23);
