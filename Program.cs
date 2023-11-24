63fc9cfd9b251085f84171af9a68e6e71a617ead
Paweł Łęczycki <pawel@msd.info.pl>
2023-11-24 13:41:07 +0100

    Memento

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿
 
-using Altkom._22_24._11._2023.DesignPatterns.Behavioral.Observer;
+using Altkom._22_24._11._2023.DesignPatterns.Behavioral.Memento;
 using Altkom._22_24._11._2023.DesignPrinciples;
 using Altkom._22_24._11._2023.SOLID.L;
 
