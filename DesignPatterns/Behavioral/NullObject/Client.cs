c55b63ee086ec9555264feef98f1e1e88af20904
Paweł Łęczycki <pawel@msd.info.pl>
2023-11-24 15:49:41 +0100

    NullObject

---
+++ DesignPatterns/Behavioral/NullObject/Client.cs
@@ -0,0 +1,31 @@
+﻿namespace Altkom._22_24._11._2023.DesignPatterns.Behavioral.NullObject
+{
+    internal class Client
+    {
+        public static void Execute()
+        {
+
+            var objects = new AbstractObject[] { new RealObject(), new NullObject() };
+            var random = new Random();
+
+            while (true)
+            {
+                var value = random.Next(0, objects.Length);
+                Console.ReadLine(  );
+
+                objects[value].Method1();
+                objects[value].Method2();
+                objects[value].Method3();
+
+                /*if (objects[value] != null)
+                    objects[value].Method1();
+             
+               objects[value]?.Method2();
+                
+                if (objects[value] is not null)
+                    objects[value].Method3();*/
+            }
+
+        }
+    }
+}
---
+++ DesignPatterns/Behavioral/NullObject/NullObject.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Altkom._22_24._11._2023.DesignPatterns.Behavioral.NullObject
+{
+    internal class NullObject : AbstractObject
+    {
+        public new void Method1()
+        {
+        }
+
+        public override void Method3()
+        {
+        }
+
+        public override void Method2()
+        {
+        }
+    }
+}
