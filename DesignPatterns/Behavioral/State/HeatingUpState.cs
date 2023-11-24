4a9ef0abd2c6a3df5233c1f27863bed1b369d946
Paweł Łęczycki <pawel@msd.info.pl>
2023-11-24 14:04:28 +0100

    State

---
+++ DesignPatterns/Behavioral/State/HeatingUpState.cs
@@ -0,0 +1,22 @@
+﻿namespace Altkom._22_24._11._2023.DesignPatterns.Behavioral.State
+{
+    internal class HeatingUpState : State
+    {
+        public HeatingUpState(CoffeeMachine coffeeMachine) : base(coffeeMachine)
+        {
+            Task.Delay(5000).ContinueWith(x => CoffeeMachine.State = new IdleState(CoffeeMachine));
+        }
+
+
+
+        public override void Large()
+        {
+            System.Console.WriteLine("Nie teraz.. Rozgrzewam się..");
+        }
+
+        public override void Small()
+        {
+            Large();
+        }
+    }
+}
